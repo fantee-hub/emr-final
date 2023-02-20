f72a338ea4de4e9b5453c42664c7225e40bfd248
Tobi Faniran <tobifaniran@gmail.com>
2023-02-20 10:15:21 +0100

    first commit

---
+++ src/common-components/SwitchButton.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { CircularProgress } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { getSingleStaff, updateStaffStatus } from '../utils/api';
+import setAuthToken from '../utils/setAuthToken';
+import './../styles/SwitchButton.css';
+
+function SwitchButton({ id, user }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [staffStatus, setStaffStatus] = useState(null);
+
+  const getStaffStatus = async () => {
+    setIsLoading(true);
+    if (user) {
+      setAuthToken(user.token);
+    }
+    try {
+      const staff_id = id;
+      const { data } = await getSingleStaff(staff_id);
+      setIsLoading(false);
+      console.log(data);
+
+      if (data) {
+        setStaffStatus(data.status);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      toast.error('an error occured');
+    }
+  };
+
+  const handleChange = async (event) => {
+    if (user) {
+      setAuthToken(user.token);
+    }
+    try {
+      const staff_id = id;
+      const status = !event.target.checked ? 'TRUE' : 'FALSE';
+      console.log(status);
+      const { data } = await updateStaffStatus(staff_id, status);
+
+      console.log(data);
+      toast.success(data.message);
+      getStaffStatus();
+    } catch (error) {
+      console.log(error);
+      toast.error('an error occured');
+    }
+  };
+
+  useEffect(() => {
+    getStaffStatus();
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      {isLoading ? (
+        <CircularProgress size={20} />
+      ) : (
+        <label className="toggle">
+          <input
+            onChange={handleChange}
+            className="toggle-checkbox"
+            checked={staffStatus}
+            type="checkbox"
+          />
+          <div className="toggle-switch"></div>
+        </label>
+      )}
+    </div>
+  );
+}
+
+export default SwitchButton;
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const ADMIN_USER_ROLE = 'ADMIN';
+export const RECEPTIONIST_USER_ROLE = 'RECEPTIONIST';
+export const DOCTOR_USER_ROLE = 'DOCTOR';
+export const CASHIER_USER_ROLE = 'CASHIER';
+export const LAB_USER_ROLE = 'LAB';
+export const PHARMACIST_USER_ROLE = 'PHARMACIST';
+export const XRAY_USER_ROLE = 'XRAY';
